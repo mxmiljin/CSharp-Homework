5a2919d4b965a93e02412fca3d7bc3c4ebdb5d2a
Максим Ильин <mxmiljin@mail.ru>
2022-08-09 15:34:25 +0300

    Task 8 completed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите ваше число:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+int StartNumber = 1;
+
+if (Number1 <= StartNumber)
+{
+    Console.WriteLine("Четных чисел в этом диапазоне нет...");
+}
+
+else if (Number1 > StartNumber)
+{
+    Console.WriteLine("Четные числа в вашем диапазоне: ");
+    while (StartNumber <= Number1)
+    {
+        if (StartNumber % 2 == 0)
+        {
+            Console.Write(StartNumber);
+            Console.Write(", ");
+        }
+        StartNumber += 1;
+    }
+}
