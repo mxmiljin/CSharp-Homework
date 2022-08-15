d4719ef3bf3d0088b412f582f6bfdb9fd0899978
mxmiljin <81637481+mxmiljin@users.noreply.github.com>
2022-08-15 23:21:44 +0300

    Add files via upload

---
+++ Task_23.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше число:");
+        int input = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+        int output = 0;
+
+        Console.WriteLine("Кубические степени от 1 до " + input + " включительно: ");
+        while (count < input)
+        {
+            output = Convert.ToInt32(Math.Pow(count, 3));
+            count++;
+            Console.Write(output);
+            Console.Write(", ");
+        }
+        output = Convert.ToInt32(Math.Pow(count, 3));
+        Console.Write(output);
+        Console.Write(".");
+    }
+}
